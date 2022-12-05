dc056f60039d952f3b61937cf6781ac69c44b95d
DNG222-code <dnagy20474@iesjoanramis.org>
2022-12-05 09:51:51 +0100

    push

--- ex16.py
+++ ex16.py
@@ -1,5 +1,9 @@
-def invertit (s):
-    cadenai = ''
-    llista=list(s)
-    for i in llista:
-        
+def invertit (a):
+    b = list(a)
+    c = b[::-1]
+    r = str(c)
+    return r
+
+a = input("Escriu una paraula: ")
+b = invertit (a)
+print ("La paraula", a, "i guarda és ", b)
