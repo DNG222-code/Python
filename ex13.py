096283ad71b2ce2c05364ec22596e53e26e7bbc9
DNG222-code <116260693+DNG222-code@users.noreply.github.com>
2022-12-07 21:38:47 +0100

    Add files via upload

--- ex13.py
+++ ex13.py
@@ -4,3 +4,15 @@ def longitut(a):
     for i in a:
         l +=1
     return l
+
+# Aplicació principal
+a = input("Introdueix una cadena: ")
+b = longitut(a)
+print("La longitud ", a, "és ", b)
+c = list()
+x="a"
+while (x!='.'):
+    x = input("Introdueix el següent element de la llista: ")
+    c.append(x)
+d = longitut(c)
+print("La longitut ", c, "és ", d)
