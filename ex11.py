9c90fa2eaaa1f2b924a28c49dd718f4cbc1ec1c8
DNG222-code <dnagy20474@iesjoanramis.org>
2022-12-07 13:06:21 +0100

    change

--- ex11.py
+++ ex11.py
@@ -4,4 +4,14 @@ def gran(x,y):
     if(x>y):
        print(x, "es major que ", y)
     elif (y>x):
-        print(y, "es major que 
+        print(y, "es major que", y)
+        a=y
+    else:
+        print(x, "és igual que", y)
+    return a
+
+#Aplicació principal 
+a = int(input("Escriure el primer valor: "))
+b = int(input("Escriure el segon valor: "))
+c = gran(a,b)
+print("El major de ", a, "i",b, "és", c)
--- ex12.py
+++ ex12.py
@@ -1,4 +1,17 @@
 # Definició de funcions
-def gran_de_tres(x,z,):
+def gran_de_tres(x,y,z):
     a=x
-    if(x>z):
+    if(x>y):
+        if(x>z):
+            a = x
+        elif (z>x):
+            a = z
+        else:
+            a = x
+    elif (y>x):
+        if (y>z):
+            a = y
+        elif (z>y):
+            a = z
+        else: #
+            
