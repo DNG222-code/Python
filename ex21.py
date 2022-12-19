97d6fd22898a0cfd54895deb7f13168381cbd43e
DNG222-code <dnagy20474@iesjoanramis.org>
2022-12-19 08:52:57 +0100

    21

--- ex21.py
+++ ex21.py
@@ -1,2 +1,31 @@
-def crear_punts():
-    
+def menu(a):
+    print("""
+        Menu
+        1.Dibuix1
+        2.Dibuix2
+        3.Sortir
+        """)
+    opcio:input("Seleccioni el dibuix que vulgui: ")
+    return opcio
+
+def crear_punts(a):
+    match(a):
+        case "1":
+            print(""" .
+             . .  
+             .   .
+              . .
+              .
+              """)
+        case "2":
+            print(""" _
+            | | |
+            _ _
+             | | |
+             _ _
+             """)
+        case other:
+            print("Adéu")
+
+opcio=2
+while(opcio)
