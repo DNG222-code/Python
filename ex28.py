9ba5d9eea8b3d35018ab12885cde7475510769ed
DNG222-code <dnagy20474@iesjoanramis.org>
2023-02-08 12:49:51 +0100

    28

--- ex28.py
+++ ex28.py
@@ -1,2 +1,17 @@
-def mostrar_majors_que(a):
-    
+def llegir():
+    llista=[]
+    a = 'a'
+    while a!='.':
+        a = input ("Introdueix un numero: ")
+        if a != '.':
+            llista.append (int(a))
+    return tuple(llista)
+
+def mostrar_majors_que(a,num):
+    for e in a:
+        if e < num:
+            print (e)
+
+x = llegir()
+i = input("Introdueix el número que vol comparar: ")
+mostrar_majors_que(x,int(i))
