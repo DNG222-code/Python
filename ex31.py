51ca4ef27a8b4b76ee2e79112549018be3532e45
DNG222-code <dnagy20474@iesjoanramis.org>
2023-03-15 13:59:20 +0100

    31 i 32

---
+++ ex31.py
@@ -0,0 +1,14 @@
+def comptar_vocal():
+    x = input("Introduce una palabra: ")
+    a = 0
+    e1 = 0
+    i1 = 0
+    o = 0
+    u = 0
+    for e in x:
+        if e == 'a':
+            a+=1
+        if e == 'e':
+            e1+=1
+        if e == 'i':
+            o+=
