c63809bdbf222f14d2f2e6737e2475b6017bd335
DNG222-code <dnagy20474@iesjoanramis.org>
2023-03-29 13:44:43 +0200

    35 y 40

---
+++ ex35.py
@@ -0,0 +1,14 @@
+def llegir_paraula():
+    return(input("Introduir una paraula"))
+def comparar_paraules(a,b):
+    if a == b:
+        print("Són iguals, per tant rimen")
+    elif a[-3:] == b[-3:]:
+        print("Rimen, per que les 3 dames lletres són iguals")
+    elif a [-2:] == b[-2:]:
+        print("Rimen, un poc, 2 dames lletres són iguals")
+    elif a [-1:] == b[-1:]:
+        print("No rimen")
+a = llegir_paraula()
+b = llegir_paraula()
+comparar_paraules(a,b)
