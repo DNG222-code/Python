868d17d439d48a154a59261822899f2452973581
DNG222-code <dnagy20474@iesjoanramis.org>
2023-04-09 15:27:00 +0200

    41

--- ex41.py
+++ ex41.py
@@ -1,6 +1,14 @@
 def suma_de_dos_digits():
     x = float(input("Introdueix un numero: "))
     z = float(input("Introdueix un altre numero: "))
-    b = print(x + z)
+    suma = x + z
+    print("La suma de", x, "i", z, "es:", suma)
+    
+    # Compara la suma con 0 para determinar si es par o impar
+    if suma % 2 == 0:
+        print("La suma es parella.")
+    else:
+        print("La suma es senar.")
 
+# Ejecutar la función
 suma_de_dos_digits()
