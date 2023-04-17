279dbc3f948d6063734a4af46603cea5bebdaf78
DNG222-code <dnagy20474@iesjoanramis.org>
2023-04-17 09:25:29 +0200

    push

--- ex48.py
+++ ex48.py
@@ -1,5 +1,5 @@
-def crear_lista_fixer(fitxer):
-    with open(fitxer, 'r') as f:
+def crear_lista_fixer(fitxernou):
+    with open(fitxernou, 'r') as f:
         l = f.readlines()
         lineaparaules = [linea.rstrip('\n')for linea in l]
         print(lineaparaules)
