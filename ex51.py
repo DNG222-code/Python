81bc380f2afbe794854296186d0ffe6164ed3cae
DNG222-code <dnagy20474@iesjoanramis.org>
2023-04-17 09:37:22 +0200

    51

---
+++ ex51.py
@@ -0,0 +1,16 @@
+def parells():
+    a = []
+    for i in range(2,101,2):
+        a.append(i)
+    print("Els parells d'1 a 100 són {}".format(a))
+
+def senar():
+    b = []
+    for i in range(2,101,2):
+        b.append(i)
+    print("Els parells d'1 a 100 són {}".format(b))
+
+#Programa
+
+parells()
+senar()
