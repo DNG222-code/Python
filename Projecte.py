39e57dfd17cb039c054204a21ac43ad14110ef4c
DNG222-code <dnagy20474@iesjoanramis.org>
2023-05-12 08:52:29 +0200

    push

---
+++ Projecte.py
@@ -0,0 +1,9 @@
+def menu():
+    print("""
+    1. Fica unes coordenades i blender imprimeix un objecte
+    2. Adivinar un número.
+    3. Inicia un objecte fet a blender.
+    4. Busca minas.
+    """)
+
+x = input(int("Introdueix un número de selecció: "))
