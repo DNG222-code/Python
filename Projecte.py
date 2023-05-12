d88e116a55a0e3cdcdba44764dbb770eed4ee33d
DNG222-code <dnagy20474@iesjoanramis.org>
2023-05-12 09:28:12 +0200

    push

--- Projecte.py
+++ Projecte.py
@@ -1,9 +1,18 @@
-def menu():
-    print("""
-    1. Fica unes coordenades i blender imprimeix un objecte
-    2. Adivinar un número.
-    3. Inicia un objecte fet a blender.
-    4. Busca minas.
-    """)
+from curses.ascii import isdigit
 
-x = input(int("Introdueix un número de selecció: "))
+
+def menu():
+    while True:
+        print("""
+            0. Sortir
+            1. Fica unes coordenades i blender imprimeix un objecte
+            2. Adivinar un número.
+            3. Inicia un objecte fet a blender.
+            4. Busca minas.
+    """)
+        a = input("Introdueix un número de selecció: ")
+        if isdigit(a):
+            if int(a) > -1 and int(a) < 5:
+                return a
+
+print(menu())
